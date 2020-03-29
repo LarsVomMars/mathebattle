084b486af308a56e4e83418a7dba50fd48a37dee
Lars K <lars@kroenner.eu>
2020-03-29 21:47:38 +0200

    Kinda working autosolver for a3

---
+++ mb2/autosolve.py
@@ -0,0 +1,120 @@
+# utility functions
+
+from bs4 import BeautifulSoup
+import time
+import re
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
+
+def parse_mfrac(mfrac) -> str:
+    mns = mfrac.find_all("mn")
+    num = mns[0].string
+    den = mns[1].string
+    return f"{num}/{den}"
+
+
+def parse_msup(msup) -> str:
+    base = msup.mi.string
+    exp = msup.mn.string
+    return f"{base}**{exp}"
+
+
+def parse_mo(mo) -> str:
+    return mo.string
+
+
+def parse_graph(math) -> str:
+    mrow_childs = math.mrow.findChildren("mrow", recursive=False)
+    eq = ""
+    for eq_parts in mrow_childs:
+        ctr = 0
+        if eq_parts.name == "mo":
+            eq = eq_parts.string
+        else:
+            for part in eq_parts.findChildren("mrow", recursive=False):
+                for part_val in part:
+                    if part_val.name == "mo":
+                        eq += f"{part_val.string}1*"
+                    elif part_val.name == "mn":
+                        eq += part_val.string
+                    elif part_val.name == "mfrac":
+                        if ctr == 0:
+                            eq += "+"
+                        eq += f"{parse_mfrac(part_val)}"
+                    elif part_val.name == "msup":
+                        if ctr >= 1:
+                            eq += "*"
+                        eq += f"{parse_msup(part_val)}"
+                    ctr += 1
+
+    return eq
+
+
+def parse_x0(mrow) -> int:
+    c = mrow.find_all("mn")
+    print(len(c), c)
+    if len(c):
+        return int(c[0].string)
+    return f"{c[0].string}/{c[1].string}"
+
+
+binary = FirefoxBinary("/usr/bin/firefox")
+
+driver = webdriver.Firefox(firefox_binary=binary)
+driver.get("https://mathebattle.de/")
+
+# Wait for URL to change be a battle URL
+while re.match("http[s]://mathebattle.de/edu_battles/start/[0-9]{4}", driver.current_url) == None:
+    time.sleep(1)
+
+time.sleep(1)
+
+form = driver.find_element_by_id("EduBattleStartForm")
+sel = form.find_elements_by_tag_name("input")
+sel[4].click()  # select 3
+sel[5].click()  # submit form
+
+time.sleep(1)
+
+div = driver.find_element_by_class_name("exercise_question")
+
+soup = BeautifulSoup(div.get_attribute("innerHTML"), features="html.parser")
+g, t, _ = soup.find_all("p")
+
+termElem = g.find_all("math")[1]
+f = parse_graph(termElem)
+x0 = parse_x0(t.mrow)
+
+print(f, x0)
+
+print(termElem)
+a = """
+<mrow>	
+	<mrow>
+        <mo>-</mo>
+		<mrow>
+			<msup>
+				<mrow>
+                    <mi>x</mi>
+                </mrow>
+				<mrow>
+                    <mrow>
+                        <mn>2</mn>
+                    </mrow>
+                </mrow>
+			</msup>
+		</mrow>
+	</mrow>	
+    <mrow>
+        <mrow>
+            <mo>+</mo>
+            <mn>9</mn>
+        </mrow>
+	</mrow>
+</mrow>
+"""
+
+b = BeautifulSoup(a, features="html.parser")
+print(parse_graph(b))
