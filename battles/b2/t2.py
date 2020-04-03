243b9ea9da0743b3bcd525c9c21ae5a9da597154
Lars K <lars@kroenner.eu>
2020-04-03 08:51:04 +0200

    ToDBG autosolver 2|2

--- battles/b2/t2.py
+++ battles/b2/t2.py
@@ -1,7 +1,16 @@
 from sympy import sympify, integrate, Symbol
 from sympy.solvers import solve
 
-from mbutil.util import sanitize_input, round_res
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
+from bs4 import BeautifulSoup
+
+import time
+
+from mbutil.util import sanitize_input, round_res, get_webdriver, open_page
+from mbutil.mathml import MATHML
 
 
 class Solver:
@@ -22,4 +31,30 @@ class Solver:
 
     @staticmethod
     def autosolve():
+        mml = MATHML()
+        driver = get_webdriver()
+        battle_url = open_page(driver)
+
+        time.sleep(1)
+
+        form = driver.find_element_by_id("EduBattleStartForm")
+        sel = form.find_elements_by_tag_name("input")
+        sel[3].click()  # select 2nd 
+        sel[5].click()  # submit form
+        
+        time.sleep(1)
+        
+        div = driver.find_element_by_class_name("exercise_question")
+        soup = BeautifulSoup(div.get_attribute("innerHTML"), "html.parser")
+
+        p = soup.p
+
+        math_elems = p.find_all("math")
+        f = sympify(mml.parse(math_elems[1]))
+        g = sympify(mml.parse(math_elems[3]))
+        
+        print(Solver.__solver(f, g))
+
+        driver.close()
+        # TODO: Solve -> insert
         pass
--- mbutil/util.py
+++ mbutil/util.py
@@ -1,5 +1,12 @@
 from sympy import N
 
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+
+import re
+import time
+
 
 def sanitize_input(eq: str):
     return eq.replace("^", "**").replace("e", "E")
@@ -7,3 +14,18 @@ def sanitize_input(eq: str):
 
 def round_res(res):
     return N(res).round(2)
+
+
+def open_page(driver):
+    driver.get("https://mathebattle.de")
+    while re.match("http[s]://mathebattle.de/edu_battles/start/[0-9]{4}", driver.current_url) == None:
+        time.sleep(1)
+    # time.sleep(1)
+    return driver.current_url
+
+
+def get_webdriver():
+    # TODO: Driver for other browsers ..
+    binary = FirefoxBinary("/usr/bin/firefox")
+    driver = webdriver.Firefox(firefox_binary=binary)
+    return driver
